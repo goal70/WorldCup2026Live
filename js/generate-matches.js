e784cd6b19df3079ab3f1551f2d23d0301477652
goal70 <animemix98@gmail.com>
2026-05-26 16:45:52 -0300

    Create generate-matches.js

---
+++ js/generate-matches.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const path = require("path");
+
+const groupsFolder = path.join(__dirname, "../data/groups");
+
+const files = fs.readdirSync(groupsFolder)
+  .filter(file =>
+    file.startsWith("groups-") &&
+    file.endsWith(".json")
+  );
+
+let allMatches = [];
+let newId = 1;
+
+files.forEach(file => {
+
+  const data = JSON.parse(
+    fs.readFileSync(
+      path.join(groupsFolder, file),
+      "utf8"
+    )
+  );
+
+  data.forEach(match => {
+
+    allMatches.push({
+
+      id: newId++,
+
+      group: match.group,
+      date: match.date,
+      status: match.status,
+
+      homeTeam: match.team1,
+      awayTeam: match.team2,
+
+      homeFlag: match.flag1,
+      awayFlag: match.flag2,
+
+      homeScore: null,
+      awayScore: null,
+
+      stadium: match.stadium,
+      city: match.city,
+
+      localTime: match.timeET,
+      argentinaTime: match.timeAR
+
+    });
+
+  });
+
+});
+
+fs.writeFileSync(
+  path.join(__dirname, "../data/matches.json"),
+  JSON.stringify(allMatches, null, 2)
+);
+
+console.log(
+  `Listo. Se generaron ${allMatches.length} partidos.`
+);
