9308675e3ade9305b45abb34c3bc1be658b24ebf
goal70 <animemix98@gmail.com>
2026-05-26 13:35:35 -0300

    Create index.js

---
+++ data/groups/index.js
@@ -0,0 +1,27 @@
+import groupA from "./groups-a.json";
+import groupB from "./groups-b.json";
+import groupC from "./groups-c.json";
+import groupD from "./groups-d.json";
+import groupE from "./groups-e.json";
+import groupF from "./groups-f.json";
+import groupG from "./groups-g.json";
+import groupH from "./groups-h.json";
+import groupI from "./groups-i.json";
+import groupJ from "./groups-j.json";
+import groupK from "./groups-k.json";
+import groupL from "./groups-l.json";
+
+export const groups = {
+  A: groupA,
+  B: groupB,
+  C: groupC,
+  D: groupD,
+  E: groupE,
+  F: groupF,
+  G: groupG,
+  H: groupH,
+  I: groupI,
+  J: groupJ,
+  K: groupK,
+  L: groupL
+};
