9e16fcbb17951c52968dadb5d6018e898929c352
goal70 <animemix98@gmail.com>
2026-06-09 18:22:37 -0300

    Update live.js

--- js/live.js
+++ js/live.js
@@ -1,1 +1,16 @@
+const API_BASE = "https://footbal-api.otanomix.workers.dev";
 
+async function testAPI() {
+    try {
+        const response = await fetch(`${API_BASE}/status`);
+        const data = await response.json();
+
+        console.log("API OK", data);
+        window.apiFootball = data;
+
+    } catch (error) {
+        console.error("API ERROR", error);
+    }
+}
+
+testAPI();
